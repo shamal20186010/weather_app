446a8bb229dca993a6043abab84e013bdd689220
Shamal Wassana <shamalwassana78@gmail.com>
2024-09-09 20:47:15 +0530

    final

---
+++ script.js
@@ -0,0 +1,137 @@
+const cityInput = document.querySelector('.city-input')
+const searchBtn = document.querySelector('.search-btn')
+
+const weatherInfoSection = document.querySelector('.wheather-info')
+const notFoundSection = document.querySelector('.not-found')
+const searchCitySecton = document.querySelector('.search-city')
+
+const countryTxt = document.querySelector('.country-txt')
+const tempTxt = document.querySelector('.temp-txt')
+const conditionTxt = document.querySelector('.condition-txt')
+const humidityValueTxt = document.querySelector('.humidity-value-txt')
+const windValueTxt = document.querySelector('.wind-value-txt')
+const weatherSummerImg = document.querySelector('.wheather-summary-img')
+const currentDadeTxt = document.querySelector('.current-date-txt')
+
+const forecastItemContainer = document.querySelector('.forecast-items-container')
+
+const apiKey = '5fc5f7e6eb9ba6ed9f3ee388d3313c10';
+
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter' && cityInput.value.trim() != '') {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`
+
+    const response = await fetch(apiUrl)
+
+    return response.json()
+}
+
+function getWeatherIcon(id) {
+    if (id <= 232) return 'thunderstorm.svg'
+    if (id <= 321) return 'drizzle.svg'
+    if (id <= 531) return 'rain.svg'
+    if (id <= 622) return 'snow.svg'
+    if (id <= 781) return 'atmosphere.svg'
+    if (id <= 800) return 'clear.svg'
+    else return 'clouds.svg'
+}
+
+function getCurrentDate() {
+    const currentDate = new Date()
+    const option = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short'
+    }
+    return currentDate.toLocaleDateString('en-GB', option)
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection)
+        return
+    }
+
+    const {
+        name: country,
+        main: { temp, humidity },
+        weather: [{ id, main }],
+        wind: { speed }
+    } = weatherData
+
+    countryTxt.textContent = country
+    tempTxt.textContent = Math.round(temp) + ' ℃'
+    conditionTxt.textContent = main
+    humidityValueTxt.textContent = humidity + '%'
+    windValueTxt.textContent = speed + ' M/s'
+
+    currentDadeTxt.textContent = getCurrentDate()
+    weatherSummerImg.src = `assets/weather/${getWeatherIcon(id)}`
+
+    await updateForecastsInfo(city)
+    showDisplaySection(weatherInfoSection)
+}
+
+async function updateForecastsInfo(city) {
+    const forecastsData = await getFetchData('forecast', city)
+
+    const timeTaken = '12:00:00'
+    const todayDate = new Date().toISOString().split('T')[0]
+
+    forecastItemContainer.innerHTML = ''
+    forecastsData.list.forEach(forecastWeather => {
+        if (forecastWeather.dt_txt.includes(timeTaken) &&
+            !forecastWeather.dt_txt.includes(todayDate)) {
+            updateForecastItems(forecastWeather)
+        }
+    })
+
+}
+
+function updateForecastItems(weatherData) {
+    const {
+        dt_txt: date,
+        weather: [{ id }],
+        main: { temp }
+    } = weatherData
+
+    const dateTaken = new Date(date)
+    const dateOption = {
+        day: '2-digit',
+        month: 'short'
+    }
+    const dateResult = dateTaken.toLocaleDateString('en-US', dateOption)
+
+    const forrecastItem = `
+        <div class="forecast-item">
+            <h5 class="forecast-item-date regular-txt">${dateResult}</h5>
+            <img src="assets/weather/${getWeatherIcon(id)}" class="forecast-item-img">
+            <h5 class="forecast-itemp-temp">${Math.round(temp)} ℃</h5>
+        </div>
+    `
+    forecastItemContainer.insertAdjacentHTML('beforeend', forrecastItem)
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchCitySecton, notFoundSection]
+        .forEach(section => section.style.display = 'none')
+
+    section.style.display = 'flex'
+}
